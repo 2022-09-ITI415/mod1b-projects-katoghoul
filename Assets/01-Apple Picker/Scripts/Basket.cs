897d6f2e7937541bbc63b6439cc78d71170f9ee6
Kaylyn Sabater <112517334+katoghoul@users.noreply.github.com>
2022-09-22 21:19:50 -0400

    Class End

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {[Header("Set Dynamically")]
-public Text scoreGT;
+public text scoreGT;
 
 void Start() {
     GameObject scoreGO = GameObject.Find("ScoreCounter");
