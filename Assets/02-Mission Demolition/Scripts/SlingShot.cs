a07f7f2e183f7813b0e6065006a891584afbd6af
Kato Sabater <112517334+katoghoul@users.noreply.github.com>
2022-09-29 13:40:51 -0400

    Added Bounce Ball (:

--- Assets/02-Mission Demolition/Scripts/SlingShot.cs
+++ Assets/02-Mission Demolition/Scripts/SlingShot.cs
@@ -62,6 +62,7 @@ public class SlingShot : MonoBehaviour {
         aimingMode = false;
         projectileRigidbody.isKinematic = false;
         projectileRigidbody.velocity = -mouseDelta * velocityMult;
+        FollowCam.POI = projectile;
         projectile = null;}
 }
 }
